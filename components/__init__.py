39326b100619c2153e54c0b50f1173b69374dc2c
Avery Lor <averywlor@gmail.com>
2025-02-16 07:17:55 -0500

    commit

--- components/__init__.py
+++ components/__init__.py
@@ -1,7 +1,6 @@
 # Necessary file to access the components 
 # __init__.py (This file makes 'components' a package)
 from .navbar import navbar
-from .songInfo import show as songInfo_show
 from .history import show as history_show
 from .about import show as about_show
 from .dataPage import show as dataPage_show
